b75ec017ae62b8c9cafeef748c0f75371ec10f3e
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-16 17:34:13 +0100

    defined some operations on tasks

--- prolog/monitoring.pl
+++ prolog/monitoring.pl
@@ -1,29 +1,47 @@
 :- module(monitoring,
 	[  
-		completed_task/1,			% ?Task
-		difference_task/2,			% ?Task, ?Diff
-		assert_task/3,				% +Name, +Estimation, +Done
-		retract_task/1,				% +Name
-		update_task/2				% +Name, +Done
+		completed_task/2,
+		earliest_task/3,
+		latest_task/3,
+		difference_task/3,
+		difference_task/4,
+		assert_task/3,
+		retract_task/5,
+		update_task/3
 	]).
 
 
-completed_task(Task) :- task(Task, Done, Done).
+completed_task(Id, Name) :- task(Id, Name, Done, Done, _).
 
-difference_task(Task, Diff) :-
-	task(Task, Estimation, Done), Diff is Estimation - Done.
+earliest_task(Id, Name, Time) :-
+	task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time > Time2).
+
+latest_task(Id, Name, Time) :-
+	task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time < Time2).
+
+difference_task(Id, Name, Diff) :-
+	oldest_task(Id, Name, TimeStamp),
+	difference_task(Id, Name, Diff, TimeStamp).
+
+difference_task(Id, Name, Diff, TimeStamp) :-
+	task(Id, Name, Estimation, Done, TimeStamp),
+	Diff is Estimation - Done.
 
 
 
-:- dynamic task/3.
+:- dynamic task/5.
+% task(Id, Name, Estimation, Done, TimeStamp).
 
-assert_task(Name, Estimation, Done) :-
-	assert(task(Name, Estimation, Done)).
+assert_task(Id, Name, Estimation) :-
+	get_time(TimeStamp),
+	assert(task(Id, Name, Estimation, 0, TimeStamp)).
 	  
-retract_task(Name) :-
-	retractall(task(Name, _, _)).
+retract_task(Id, Name, Estimation, Done, TimeStamp) :-
+	retractall(task(Id, Name, Estimation, Done, TimeStamp)).
 
-update_task(Name, Done) :-
-	task(Name, Estimation, _),
-	retract_task(Name),
-	assert_task(Name, Estimation, Done).
+update_task(Id, Name, Done) :-
+	task(Id, Name, Estimation, _, _),
+	get_time(TimeStamp),
+	assert_task(Id, Name, Estimation, Done, TimeStamp).
