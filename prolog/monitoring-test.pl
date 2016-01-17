3f29e36c8bbf9826435647c40796031006e3c188
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-17 16:44:48 +0100

    completed classify_iteration clause first draft

--- prolog/monitoring-test.pl
+++ prolog/monitoring-test.pl
@@ -1,48 +1,58 @@
 :- use_module(monitoring).
 
 % Testing facts
+:-	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Start),
+	date_time_stamp(date(2006,7,20,0,0,0,0,-,-), End),
+	assert_iteration(1, Start, End).
+
 % Day 1
 :-	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(1, "Logging", 10, 0, Stamp)).
+	assert(task(1, "Logging", 10, 0, Stamp)).
 :-	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(2, "Marketing", 7, 0, Stamp)).
+	assert(task(2, "Marketing", 7, 0, Stamp)).
 :-	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 0, Stamp)).
+	assert(task(3, "Developing", 13, 0, Stamp)).
 
 % Day 2
 :-	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(1, "Logging", 10, 2, Stamp)).
+	assert(task(1, "Logging", 10, 2, Stamp)).
 :-	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(2, "Marketing", 7, 3, Stamp)).
+	assert(task(2, "Marketing", 7, 3, Stamp)).
 :-	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 3, Stamp)).
+	assert(task(3, "Developing", 13, 3, Stamp)).
 
 % Day 3
 :-	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(1, "Logging", 10, 5, Stamp)).
+	assert(task(1, "Logging", 10, 5, Stamp)).
 :-	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(2, "Marketing", 7, 5, Stamp)).
+	assert(task(2, "Marketing", 7, 5, Stamp)).
 :-	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 6, Stamp)).
+	assert(task(3, "Developing", 13, 6, Stamp)).
 
 %Day 4
 :-	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(1, "Logging", 10, 8, Stamp)).
+	assert(task(1, "Logging", 10, 8, Stamp)).
 :-	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(2, "Marketing", 7, 7, Stamp)).
+	assert(task(2, "Marketing", 7, 7, Stamp)).
 :-	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 7, Stamp)).
+	assert(task(3, "Developing", 13, 7, Stamp)).
 
 % Day 5
 :-	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(1, "Logging", 10, 10, Stamp)).
+	assert(task(1, "Logging", 10, 10, Stamp)).
 :-	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 8, Stamp)).
+	assert(task(3, "Developing", 13, 8, Stamp)).
 
 % Day 6
 :-	date_time_stamp(date(2006,7,19,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 10, Stamp)).
+	assert(task(3, "Developing", 13, 10, Stamp)).
 
 % Day 7
 :-	date_time_stamp(date(2006,7,20,0,0,0,0,-,-), Stamp),
-	assert(monitoring:task(3, "Developing", 13, 11, Stamp)).
+	assert(task(3, "Developing", 13, 11, Stamp)).
+
+% Add to iteration
+:-	foreach(
+		task(Id, Name, Est, Done, Time),
+		iteration_task(1, task(Id, Name, Est, Done, Time))
+	).
--- prolog/monitoring.pl
+++ prolog/monitoring.pl
@@ -1,38 +1,119 @@
 :- module(monitoring,
 	[  
+		task/5,
+		iteration/4,
+		discrepancy/1,
+		classify_iteration/2,
+		historical_completeness/1,
+		iteration_to_complete/2,
+		iteration_latest_tasks/2,
+		completed_tasks/2,
+		completed_task/1,
 		completed_task/2,
 		earliest_task/3,
+		latest_task/1,
 		latest_task/3,
 		difference_task/3,
 		difference_task/4,
 		assert_task/3,
 		retract_task/5,
-		update_task/3
+		update_task/3,
+		assert_iteration/3,
+		retract_iteration/1,
+		iteration_task/2
 	]).
 
+:- use_module(library(lambda)).
 
-completed_task(Id, Name) :- task(Id, Name, Done, Done, _).
+
+discrepancy(high).
+discrepancy(low).
+
+classify_iteration(IdIteration, Discrepancy) :-
+	iteration_to_complete(IdIteration, ToComplete),
+	historical_completeness(Completeness),
+	discrepancies(ToComplete, Completeness, [], Discrepancies),
+	sumlist(Discrepancies, Sum),
+	(
+		length(Discrepancies, Len),
+		Sum >= Len,
+		Discrepancy = discrepancy(high), !
+	);(
+		Discrepancy = discrepancy(low)
+	).
+
+discrepancies(_, [], Discrepancies, Discrepancies).
+discrepancies(ToComplete, [Value|Values], Acc, Discrepancies) :-
+	Discrepancy is ToComplete + abs(Value),
+	append([Discrepancy], Acc, NewAcc),
+	discrepancies(ToComplete, Values, NewAcc, Discrepancies).
+
+historical_completeness(Values) :-
+	get_time(Today),
+	findall(It, (iteration(It, _, End, _), End < Today), Its),
+	historical_completeness(Its, [], Values).
+historical_completeness([], Values, Values).
+historical_completeness([It|Its], Acc, Values) :-
+	iteration_to_complete(It, ToComplete),
+	append([ToComplete], Acc, NewAcc),
+	historical_completeness(Its, NewAcc, Values).
+
+iteration_to_complete(IdIteration, ToComplete) :-
+	iteration_latest_tasks(IdIteration, Tasks),
+	calc_iteration_to_complete(Tasks, 0, ToComplete).
+calc_iteration_to_complete([], ToComplete, ToComplete).
+calc_iteration_to_complete([Task|Tasks], Acc, ToComplete) :-
+	difference_task(Task, Diff),
+	NewAcc is Diff + Acc,
+	calc_iteration_to_complete(Tasks, NewAcc, ToComplete).
+
+iteration_latest_tasks(IdIteration, Tasks) :-
+	iteration(IdIteration, _, _, AllTasks),
+	include(latest_task, AllTasks, Tasks).
+
+completed_tasks(IdIteration, CompletedTasks) :-
+	iteration_latest_tasks(IdIteration, Tasks),
+	include(completed_task, Tasks, CompletedTasks).
+
+completed_task(Task) :-
+	Task = task(Id, Name, _, _, _),
+	completed_task(Id, Name).
+
+completed_task(Id, Name) :-
+	task(Id, Name, Done, Done, _).
+
+completed_task(Id, Name, Time) :-
+	task(Id, Name, Done, Done, Time).
 
 earliest_task(Id, Name, Time) :-
 	task(Id, Name, _, _, Time),
 	\+ (task(Id, Name, _, _, Time2), Time > Time2).
 
+latest_task(Task) :-
+	Task = task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time < Time2).
+
 latest_task(Id, Name, Time) :-
 	task(Id, Name, _, _, Time),
 	\+ (task(Id, Name, _, _, Time2), Time < Time2).
 
-difference_task(Id, Name, Diff) :-
-	oldest_task(Id, Name, TimeStamp),
-	difference_task(Id, Name, Diff, TimeStamp).
+difference_task(Task, Diff) :-
+	Task = task(Id, Name, _, _, Time),
+	difference_task(Id, Name, Time, Diff).
 
-difference_task(Id, Name, Diff, TimeStamp) :-
+difference_task(Id, Name, Diff) :-
+	latest_task(Id, Name, TimeStamp),
+	difference_task(Id, Name, TimeStamp, Diff).
+
+difference_task(Id, Name, TimeStamp, Diff) :-
 	task(Id, Name, Estimation, Done, TimeStamp),
 	Diff is Estimation - Done.
 
 
 
-:- dynamic task/5.
+:- dynamic task/5, iteration/4.
 % task(Id, Name, Estimation, Done, TimeStamp).
+% iteration(Id, Start, End, Tasks).
 
 assert_task(Id, Name, Estimation) :-
 	get_time(TimeStamp),
@@ -45,3 +126,20 @@ update_task(Id, Name, Done) :-
 	task(Id, Name, Estimation, _, _),
 	get_time(TimeStamp),
 	assert_task(Id, Name, Estimation, Done, TimeStamp).
+
+assert_iteration(Id, Start, End) :-
+	assert(iteration(Id, Start, End, [])).
+
+retract_iteration(IdIteration) :-
+	retractall(iteration(IdIteration, _, _)).
+
+iteration_task(IdIteration, Task) :-
+	compound(Task),
+	iteration(IdIteration, Start, End, Tasks),
+	\+ (member(Task, Tasks)),
+	Task =.. [task, _, _, _, _, Time],
+	Time >= Start, Time =< End,
+	append(Tasks, [Task], NewTasks),
+	assert(iteration(IdIteration, Start, End, NewTasks)),
+	retractall(iteration(IdIteration, Start, End, Tasks)).
+
