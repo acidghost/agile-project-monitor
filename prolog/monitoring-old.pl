4b777b4f281b7b7db48aa8771e738f7ce282b638
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-25 22:12:56 +0100

    started rewriting inference engine & DB

---
+++ prolog/monitoring-old.pl
@@ -0,0 +1,172 @@
+:- module(monitoring,
+	[  
+		task/5,
+		iteration/4,
+		discrepancy/1,
+		project/5,
+		classify_iteration/2,
+		historical_completeness/1,
+		iteration_to_complete/2,
+		iteration_latest_tasks/2,
+		completed_tasks/2,
+		completed_task/1,
+		completed_task/2,
+		earliest_task/3,
+		latest_task/1,
+		latest_task/3,
+		difference_task/3,
+		difference_task/4,
+		assert_task/3,
+		retract_task/5,
+		update_task/3,
+		assert_iteration/3,
+		retract_iteration/1,
+		iteration_task/2,
+		assert_project/4,
+		retract_project/2,
+		project_iteration/3
+	]).
+
+:- use_module(library(lambda)).
+
+
+discrepancy(high).
+discrepancy(low).
+
+classify_iteration(IdIteration, Discrepancy) :-
+	iteration_to_complete(IdIteration, ToComplete),
+	historical_completeness(Completeness),
+	discrepancies(ToComplete, Completeness, [], Discrepancies),
+	sumlist(Discrepancies, Sum),
+	(
+		length(Discrepancies, Len),
+		Sum >= Len,
+		Discrepancy = discrepancy(high), !
+	);(
+		Discrepancy = discrepancy(low)
+	).
+
+discrepancies(_, [], Discrepancies, Discrepancies).
+discrepancies(ToComplete, [Value|Values], Acc, Discrepancies) :-
+	Discrepancy is ToComplete + abs(Value),
+	append([Discrepancy], Acc, NewAcc),
+	discrepancies(ToComplete, Values, NewAcc, Discrepancies).
+
+historical_completeness(Values) :-
+	get_time(Today),
+	findall(It, (iteration(It, _, End, _), End < Today), Its),
+	historical_completeness(Its, [], Values).
+historical_completeness([], Values, Values).
+historical_completeness([It|Its], Acc, Values) :-
+	iteration_to_complete(It, ToComplete),
+	append([ToComplete], Acc, NewAcc),
+	historical_completeness(Its, NewAcc, Values).
+
+iteration_to_complete(IdIteration, ToComplete) :-
+	iteration_latest_tasks(IdIteration, Tasks),
+	calc_iteration_to_complete(Tasks, 0, ToComplete).
+calc_iteration_to_complete([], ToComplete, ToComplete).
+calc_iteration_to_complete([Task|Tasks], Acc, ToComplete) :-
+	difference_task(Task, Diff),
+	NewAcc is Diff + Acc,
+	calc_iteration_to_complete(Tasks, NewAcc, ToComplete).
+
+iteration_latest_tasks(IdIteration, Tasks) :-
+	iteration(IdIteration, _, _, AllTasks),
+	include(latest_task, AllTasks, Tasks).
+
+completed_tasks(IdIteration, CompletedTasks) :-
+	iteration_latest_tasks(IdIteration, Tasks),
+	include(completed_task, Tasks, CompletedTasks).
+
+completed_task(Task) :-
+	Task = task(Id, Name, _, _, _),
+	completed_task(Id, Name).
+
+completed_task(Id, Name) :-
+	task(Id, Name, Done, Done, _).
+
+completed_task(Id, Name, Time) :-
+	task(Id, Name, Done, Done, Time).
+
+earliest_task(Id, Name, Time) :-
+	task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time > Time2).
+
+latest_task(Task) :-
+	Task = task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time < Time2).
+
+latest_task(Id, Name, Time) :-
+	task(Id, Name, _, _, Time),
+	\+ (task(Id, Name, _, _, Time2), Time < Time2).
+
+difference_task(Task, Diff) :-
+	Task = task(Id, Name, _, _, Time),
+	difference_task(Id, Name, Time, Diff).
+
+difference_task(Id, Name, Diff) :-
+	latest_task(Id, Name, TimeStamp),
+	difference_task(Id, Name, TimeStamp, Diff).
+
+difference_task(Id, Name, TimeStamp, Diff) :-
+	task(Id, Name, Estimation, Done, TimeStamp),
+	Diff is Estimation - Done.
+
+
+
+:- dynamic task/5, iteration/4, project/5.
+% task(Id, Name, Estimation, Done, TimeStamp).
+% iteration(Id, Start, End, Tasks).
+% project(Id, Name, Start, End, Iterations).
+
+assert_task(Id, Name, Estimation) :-
+	get_time(TimeStamp),
+	assert(task(Id, Name, Estimation, 0, TimeStamp)).
+	  
+retract_task(Id, Name, Estimation, Done, TimeStamp) :-
+	retractall(task(Id, Name, Estimation, Done, TimeStamp)).
+
+update_task(Id, Name, Done) :-
+	task(Id, Name, Estimation, _, _),
+	get_time(TimeStamp),
+	assert_task(Id, Name, Estimation, Done, TimeStamp).
+
+assert_iteration(Id, Start, End) :-
+	assert(iteration(Id, Start, End, [])).
+
+retract_iteration(IdIteration) :-
+	retractall(iteration(IdIteration, _, _)).
+
+iteration_task(IdIteration, Task) :-
+	compound(Task),
+	iteration(IdIteration, Start, End, Tasks),
+	\+ (member(Task, Tasks)),
+	Task =.. [task, _, _, _, _, Time],
+	Time >= Start, Time =< End,
+	append(Tasks, [Task], NewTasks),
+	assert(iteration(IdIteration, Start, End, NewTasks)),
+	retractall(iteration(IdIteration, Start, End, Tasks)).
+
+assert_project(Id, Name, Start, End) :-
+	assert(project(Id, Name, Start, End, [])).
+
+retract_project(Id, Name) :-
+	retractall(project(Id, Name, _, _)).
+
+project_iteration(Id, Name, Iteration) :-
+	compound(Iteration),
+	project(Id, Name, Start, End, Iterations),
+	Iteration =.. [iteration, _, IStart, IEnd, _],
+	IStart < IEnd,
+	Start =< IStart, End >= IStart,
+	Start =< IEnd, End >= IEnd,
+	\+ (member(Iteration, Iterations)),
+	append(Iterations, [Iteration], NewIterations),
+	assert(project(Id, Name, Start, End, NewIterations)),
+	retractall(project(Id, Name, Start, End, Iterations)).
+
+
+%% REMOVE ME!
+:- consult('./monitoring-test-old.pl').
+
--- prolog/monitoring.pl
+++ prolog/monitoring.pl
@@ -1,172 +1,99 @@
 :- module(monitoring,
-	[  
-		task/5,
-		iteration/4,
-		discrepancy/1,
-		project/5,
-		classify_iteration/2,
-		historical_completeness/1,
-		iteration_to_complete/2,
-		iteration_latest_tasks/2,
-		completed_tasks/2,
-		completed_task/1,
-		completed_task/2,
-		earliest_task/3,
-		latest_task/1,
-		latest_task/3,
-		difference_task/3,
-		difference_task/4,
-		assert_task/3,
-		retract_task/5,
-		update_task/3,
-		assert_iteration/3,
-		retract_iteration/1,
-		iteration_task/2,
-		assert_project/4,
-		retract_project/2,
-		project_iteration/3
+	[
+		velocity_chart/5
 	]).
 
-:- use_module(library(lambda)).
 
+velocity_chart(ProjectId, Current, Mean, Percentile66, Percentile33) :-
+	project_iterations(ProjectId, Its),
+	current_velocity_difference(ProjectId, Current),
+	velocity_chart(Its, [], Mean, Percentile66, Percentile33).
+velocity_chart([It|Its], Differences, Mean, Percentile66, Percentile33) :-
+	iteration(It, Start, End),
+	velocity_difference(It, Difference),
+	append(Difference, Differences, NewDifferences),
+	velocity_chart(Its, NewDifferences, Mean, Percentile66, Percentile33).
+velocity_chart([], Differences, Mean, Percentile66, Percentile33) :-
+	msort(Differences, Sorted),
+	Mean is 0, Percentile66 is 1, Percentile33 is 2.
 
-discrepancy(high).
-discrepancy(low).
-
-classify_iteration(IdIteration, Discrepancy) :-
-	iteration_to_complete(IdIteration, ToComplete),
-	historical_completeness(Completeness),
-	discrepancies(ToComplete, Completeness, [], Discrepancies),
-	sumlist(Discrepancies, Sum),
-	(
-		length(Discrepancies, Len),
-		Sum >= Len,
-		Discrepancy = discrepancy(high), !
-	);(
-		Discrepancy = discrepancy(low)
+project_iterations(ProjectId, ItIds) :-
+	project(ProjectId, _, PjStart, PjEnd, _, _),
+	findall(ItId, (
+			project_iteration(ProjectId, ItId),
+			iteration(ItId, ItStart, ItEnd),
+			ItStart >= PjStart, ItStart < PjEnd,
+			ItEnd > PjStart, ItEnd =< PjEnd),
+		ItIds
 	).
 
-discrepancies(_, [], Discrepancies, Discrepancies).
-discrepancies(ToComplete, [Value|Values], Acc, Discrepancies) :-
-	Discrepancy is ToComplete + abs(Value),
-	append([Discrepancy], Acc, NewAcc),
-	discrepancies(ToComplete, Values, NewAcc, Discrepancies).
+iteration_updates(ItId, UpdateIds) :-
+	iteration(ItId, Start, End),
+	findall(UpdateId, (
+			task_update(UpdateId, _, When, _, _),
+			When >= Start, When =< End),
+		UpdateIds
+	).
 
-historical_completeness(Values) :-
-	get_time(Today),
-	findall(It, (iteration(It, _, End, _), End < Today), Its),
-	historical_completeness(Its, [], Values).
-historical_completeness([], Values, Values).
-historical_completeness([It|Its], Acc, Values) :-
-	iteration_to_complete(It, ToComplete),
-	append([ToComplete], Acc, NewAcc),
-	historical_completeness(Its, NewAcc, Values).
+iteration_updates(ItId, When, UpdateIds) :-
+	findall(UpdateId, task_update(UpdateId, _, When, _, _), UpdateIds). 
 
-iteration_to_complete(IdIteration, ToComplete) :-
-	iteration_latest_tasks(IdIteration, Tasks),
-	calc_iteration_to_complete(Tasks, 0, ToComplete).
-calc_iteration_to_complete([], ToComplete, ToComplete).
-calc_iteration_to_complete([Task|Tasks], Acc, ToComplete) :-
-	difference_task(Task, Diff),
-	NewAcc is Diff + Acc,
-	calc_iteration_to_complete(Tasks, NewAcc, ToComplete).
+latest_iteration(ProjectId, ItId, Start, End) :-
+	project_iteration(ProjectId, ItId),
+	iteration(ItId, Start, End),
+	\+ (project_iteration(ProjectId, ItId2), iteration(ItId2, _, End2), ItId \= ItId2, End < End2).
 
-iteration_latest_tasks(IdIteration, Tasks) :-
-	iteration(IdIteration, _, _, AllTasks),
-	include(latest_task, AllTasks, Tasks).
+velocity_difference(ItId, Difference) :-
+	total_initial_estimation(ItId, TotInitialEstimation),
+	total_remaining_effort(ItId, TotRemainingEffort),
+	Difference is abs(TotInitialEstimation - TotRemainingEffort).
 
-completed_tasks(IdIteration, CompletedTasks) :-
-	iteration_latest_tasks(IdIteration, Tasks),
-	include(completed_task, Tasks, CompletedTasks).
+current_velocity_difference(ProjectId, Current) :-
+	latest_iteration(ProjectId, ItId, _, _),
+	velocity_difference(ItId, Current).
 
-completed_task(Task) :-
-	Task = task(Id, Name, _, _, _),
-	completed_task(Id, Name).
+collect_initial_estimations(ItId, When, InitialEstimations) :-
+	iteration_updates(ItId, When, UpdateIds),
+	findall(InitialEstimation, (
+		task_update(UpdateId, _, When, InitialEstimation, _),
+		member(UpdateId, UpdateIds)
+	), InitialEstimations).
 
-completed_task(Id, Name) :-
-	task(Id, Name, Done, Done, _).
+collect_remaining_effort(ItId, When, RemainingEfforts) :-
+	iteration_updates(ItId, When, UpdateIds),
+	findall(Remaining, (
+		task_update(UpdateId, _, When, Remaining, _),
+		member(UpdateId, UpdateIds)
+	), RemainingEfforts).
 
-completed_task(Id, Name, Time) :-
-	task(Id, Name, Done, Done, Time).
+total_initial_estimation(ItId, TotInitialEstimation) :-
+	iteration(ItId, Start, _),
+	collect_initial_estimations(ItId, Start, InitialEstimations),
+	sum(InitialEstimations, TotInitialEstimation).
 
-earliest_task(Id, Name, Time) :-
-	task(Id, Name, _, _, Time),
-	\+ (task(Id, Name, _, _, Time2), Time > Time2).
+total_remaining_effort(ItId, TotRemainingEffort) :-
+	iteration(ItId, _, End),
+	collect_remaining_effort(ItId, End, RemainingEfforts),
+	sum(RemainingEfforts, TotRemainingEffort).
 
-latest_task(Task) :-
-	Task = task(Id, Name, _, _, Time),
-	\+ (task(Id, Name, _, _, Time2), Time < Time2).
-
-latest_task(Id, Name, Time) :-
-	task(Id, Name, _, _, Time),
-	\+ (task(Id, Name, _, _, Time2), Time < Time2).
-
-difference_task(Task, Diff) :-
-	Task = task(Id, Name, _, _, Time),
-	difference_task(Id, Name, Time, Diff).
-
-difference_task(Id, Name, Diff) :-
-	latest_task(Id, Name, TimeStamp),
-	difference_task(Id, Name, TimeStamp, Diff).
-
-difference_task(Id, Name, TimeStamp, Diff) :-
-	task(Id, Name, Estimation, Done, TimeStamp),
-	Diff is Estimation - Done.
+iteration_day_difference(ItId, When, Difference) :-
+	collect_initial_estimations(ItId, When, InitialEstimations),
+	collect_remaining_effort(ItId, When, Remainings),
+	sum(InitialEstimations, TotEstimation),
+	sum(Remainings, TotRemaining),
+	Difference is TotEstimation - TotRemaining.
 
 
+:- dynamic	project/6,
+			iteration/3,
+			project_iteration/2,
+			task/4,
+			task_update/5,
+			project_task/2.
 
-:- dynamic task/5, iteration/4, project/5.
-% task(Id, Name, Estimation, Done, TimeStamp).
-% iteration(Id, Start, End, Tasks).
-% project(Id, Name, Start, End, Iterations).
-
-assert_task(Id, Name, Estimation) :-
-	get_time(TimeStamp),
-	assert(task(Id, Name, Estimation, 0, TimeStamp)).
-	  
-retract_task(Id, Name, Estimation, Done, TimeStamp) :-
-	retractall(task(Id, Name, Estimation, Done, TimeStamp)).
-
-update_task(Id, Name, Done) :-
-	task(Id, Name, Estimation, _, _),
-	get_time(TimeStamp),
-	assert_task(Id, Name, Estimation, Done, TimeStamp).
-
-assert_iteration(Id, Start, End) :-
-	assert(iteration(Id, Start, End, [])).
-
-retract_iteration(IdIteration) :-
-	retractall(iteration(IdIteration, _, _)).
-
-iteration_task(IdIteration, Task) :-
-	compound(Task),
-	iteration(IdIteration, Start, End, Tasks),
-	\+ (member(Task, Tasks)),
-	Task =.. [task, _, _, _, _, Time],
-	Time >= Start, Time =< End,
-	append(Tasks, [Task], NewTasks),
-	assert(iteration(IdIteration, Start, End, NewTasks)),
-	retractall(iteration(IdIteration, Start, End, Tasks)).
-
-assert_project(Id, Name, Start, End) :-
-	assert(project(Id, Name, Start, End, [])).
-
-retract_project(Id, Name) :-
-	retractall(project(Id, Name, _, _)).
-
-project_iteration(Id, Name, Iteration) :-
-	compound(Iteration),
-	project(Id, Name, Start, End, Iterations),
-	Iteration =.. [iteration, _, IStart, IEnd, _],
-	IStart < IEnd,
-	Start =< IStart, End >= IStart,
-	Start =< IEnd, End >= IEnd,
-	\+ (member(Iteration, Iterations)),
-	append(Iterations, [Iteration], NewIterations),
-	assert(project(Id, Name, Start, End, NewIterations)),
-	retractall(project(Id, Name, Start, End, Iterations)).
-
-
-%% REMOVE ME!
-:- consult('./monitoring-test.pl').
-
+% project(Id, Name, Start, End, IterationSize, Unit)
+% iteration(Id, Start, End)
+% project_iteration(ProjectId, IterationId)
+% task(Id, Name, Story, Labels)
+% task_update(UpdateId, TaskId, When, Initial, Remaining)
+% project_task(ProjectId, TaskId)
