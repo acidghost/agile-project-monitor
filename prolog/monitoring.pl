e543e4a879a8fe172e97406c647bc32aa5db6286
Andrea Jemmett <andreajemmett@gmail.com>
2016-01-27 01:24:01 +0100

    done velocity & brndown charts (Prolog)

--- prolog/monitoring.pl
+++ prolog/monitoring.pl
@@ -1,21 +1,83 @@
 :- module(monitoring,
 	[
-		velocity_chart/5
+		velocity_chart/5,
+		burndown_chart/3,
+
+		project/6,
+		iteration/3,
+		project_iteration/2,
+		task/4,
+		task_update/5,
+		project_task/2
 	]).
 
 
+:- dynamic
+	project/6,
+	iteration/3,
+	project_iteration/2,
+	task/4,
+	task_update/5,
+	project_task/2.
+
+% project(Id, Name, Start, End, IterationSize, Unit)
+% iteration(Id, Start, End)
+% project_iteration(ProjectId, IterationId)
+% task(Id, Name, Story, Labels)
+% task_update(UpdateId, TaskId, When, Initial, Remaining)
+% project_task(ProjectId, TaskId)
+
+
+burndown_status(Difference, Status) :- Difference =< -0.66, Status = behind, !.
+burndown_status(Difference, Status) :- Difference > -0.66, Difference < 0.66, Status = on_schedule, !.
+burndown_status(Difference, Status) :- Difference >= 0.66, Status = ahead.
+
+burndown_chart(ItId, Differences, Statuses) :-
+	iteration_days(ItId, Days),
+	burndown_chart(ItId, Days, [], Differences, Statuses).
+burndown_chart(ItId, [Day|Days], Acc, Differences, Statuses) :-
+	length(Acc, ItDay),
+	iteration_day_difference(ItId, ItDay, Day, Difference),
+	append(Acc, [Difference], NewAcc),
+	burndown_chart(ItId, Days, NewAcc, Differences, Statuses).
+burndown_chart(_, [], Differences, Differences, Statuses) :-
+	maplist(burndown_status, Differences, Statuses),
+	!. % FIXME
+
 velocity_chart(ProjectId, Current, Mean, Percentile66, Percentile33) :-
 	project_iterations(ProjectId, Its),
-	current_velocity_difference(ProjectId, Current),
-	velocity_chart(Its, [], Mean, Percentile66, Percentile33).
-velocity_chart([It|Its], Differences, Mean, Percentile66, Percentile33) :-
-	iteration(It, Start, End),
-	velocity_difference(It, Difference),
-	append(Difference, Differences, NewDifferences),
-	velocity_chart(Its, NewDifferences, Mean, Percentile66, Percentile33).
-velocity_chart([], Differences, Mean, Percentile66, Percentile33) :-
+	current_remaining_effort(ProjectId, Current),
+	last(Its, Last), select(Last, Its, PreviousIts),
+	maplist(total_remaining_effort, PreviousIts, Differences),
+	mean(Differences, Mean),
 	msort(Differences, Sorted),
-	Mean is 0, Percentile66 is 1, Percentile33 is 2.
+	length(Differences, Length),
+	percentile(Sorted, Length, 33, Percentile33),
+	percentile(Sorted, Length, 66, Percentile66),
+	!.
+
+mean(Data, Mean) :-
+	sumlist(Data, Sum),
+	length(Data, Length),
+	Mean is Sum / Length.
+
+% TODO: refactor me!
+percentile(SortedData, Length, Percentile, Value) :-
+	integer((Percentile / 100) * Length) ->
+	(
+		Index is (Percentile / 100) * Length,
+		include(=<(Index), SortedData, LessThan),
+		length(LessThan, LLT),
+		nth1(LLT, SortedData, LLTth),
+		nth1(LLT+1, SortedData, LLT2th),
+		Value is (LLTth + LLT2th) / 2, !
+	);
+	(
+		Index is ceiling((Percentile / 100) * Length),
+		include(>=(Index), SortedData, LessThan),
+		length(LessThan, LLT),
+		nth1(LLT, SortedData, Value)
+	).
 
 project_iterations(ProjectId, ItIds) :-
 	project(ProjectId, _, PjStart, PjEnd, _, _),
@@ -30,70 +92,94 @@ project_iterations(ProjectId, ItIds) :-
 iteration_updates(ItId, UpdateIds) :-
 	iteration(ItId, Start, End),
 	findall(UpdateId, (
-			task_update(UpdateId, _, When, _, _),
+			project_iteration(ProjectId, ItId),
+			project_task(ProjectId, TaskId),
+			task_update(UpdateId, TaskId, When, _, _),
 			When >= Start, When =< End),
 		UpdateIds
 	).
 
 iteration_updates(ItId, When, UpdateIds) :-
-	findall(UpdateId, task_update(UpdateId, _, When, _, _), UpdateIds). 
+	iteration(ItId, _, _),
+	findall(UpdateId, (
+			project_iteration(ProjectId, ItId),
+			project_task(ProjectId, TaskId),
+			task_update(UpdateId, TaskId, When, _, _)),
+		UpdateIds
+	).
 
 latest_iteration(ProjectId, ItId, Start, End) :-
 	project_iteration(ProjectId, ItId),
 	iteration(ItId, Start, End),
-	\+ (project_iteration(ProjectId, ItId2), iteration(ItId2, _, End2), ItId \= ItId2, End < End2).
+	\+ (
+		project_iteration(ProjectId, ItId2),
+		iteration(ItId2, _, End2),
+		ItId \= ItId2, End < End2
+	).
 
-velocity_difference(ItId, Difference) :-
-	total_initial_estimation(ItId, TotInitialEstimation),
-	total_remaining_effort(ItId, TotRemainingEffort),
-	Difference is abs(TotInitialEstimation - TotRemainingEffort).
+latest_updates(ItId, UpdateIds) :-
+	iteration_updates(ItId, AllUpdates),
+	findall(UpdateId, (
+		task_update(UpdateId, TaskId, When, _, _),
+		member(UpdateId, AllUpdates),
+		\+ (
+			task_update(UpdateId2, TaskId, When2, _, _),
+			member(UpdateId2, AllUpdates),
+			UpdateId \= UpdateId2, When < When2
+		)
+	), UpdateIds).
 
-current_velocity_difference(ProjectId, Current) :-
+current_remaining_effort(ProjectId, Current) :-
 	latest_iteration(ProjectId, ItId, _, _),
-	velocity_difference(ItId, Current).
+	total_remaining_effort(ItId, Current).
 
-collect_initial_estimations(ItId, When, InitialEstimations) :-
-	iteration_updates(ItId, When, UpdateIds),
+collect_initial_estimations(ItId, UpdateIds, When, InitialEstimations) :-
+	iteration(ItId, _, _),
+	project_iteration(ProjectId, ItId),
 	findall(InitialEstimation, (
-		task_update(UpdateId, _, When, InitialEstimation, _),
+		project_task(ProjectId, TaskId),
+		task_update(UpdateId, TaskId, When, InitialEstimation, _),
 		member(UpdateId, UpdateIds)
 	), InitialEstimations).
 
-collect_remaining_effort(ItId, When, RemainingEfforts) :-
-	iteration_updates(ItId, When, UpdateIds),
+collect_remaining_effort(ItId, UpdateIds, When, RemainingEfforts) :-
+	iteration(ItId, _, _),
+	project_iteration(ProjectId, ItId),
 	findall(Remaining, (
-		task_update(UpdateId, _, When, Remaining, _),
+		project_task(ProjectId, TaskId),
+		task_update(UpdateId, TaskId, When, _, Remaining),
 		member(UpdateId, UpdateIds)
 	), RemainingEfforts).
 
 total_initial_estimation(ItId, TotInitialEstimation) :-
 	iteration(ItId, Start, _),
 	collect_initial_estimations(ItId, Start, InitialEstimations),
-	sum(InitialEstimations, TotInitialEstimation).
+	sumlist(InitialEstimations, TotInitialEstimation).
 
 total_remaining_effort(ItId, TotRemainingEffort) :-
-	iteration(ItId, _, End),
-	collect_remaining_effort(ItId, End, RemainingEfforts),
-	sum(RemainingEfforts, TotRemainingEffort).
+	latest_updates(ItId, UpdateIds),
+	collect_remaining_effort(ItId, UpdateIds, _, RemainingEfforts),
+	sumlist(RemainingEfforts, TotRemainingEffort).
 
-iteration_day_difference(ItId, When, Difference) :-
-	collect_initial_estimations(ItId, When, InitialEstimations),
-	collect_remaining_effort(ItId, When, Remainings),
-	sum(InitialEstimations, TotEstimation),
-	sum(Remainings, TotRemaining),
-	Difference is TotEstimation - TotRemaining.
+iteration_day_difference(ItId, ItDay, When, Difference) :-
+	iteration(ItId, _, _),
+	project_iteration(ProjectId, ItId),
+	project(ProjectId, _, _, _, Size, _),
+	iteration_updates(ItId, When, UpdateIds),
+	collect_initial_estimations(ItId, UpdateIds, When, InitialEstimations),
+	collect_remaining_effort(ItId, UpdateIds, When, Remainings),
+	sumlist(InitialEstimations, TotEstimation),
+	sumlist(Remainings, TotRemaining),
+	IdealRemaining is TotEstimation - ItDay * (TotEstimation / (Size - 1)),
+	Difference is IdealRemaining - TotRemaining.
 
-
-:- dynamic	project/6,
-			iteration/3,
-			project_iteration/2,
-			task/4,
-			task_update/5,
-			project_task/2.
-
-% project(Id, Name, Start, End, IterationSize, Unit)
-% iteration(Id, Start, End)
-% project_iteration(ProjectId, IterationId)
-% task(Id, Name, Story, Labels)
-% task_update(UpdateId, TaskId, When, Initial, Remaining)
-% project_task(ProjectId, TaskId)
+iteration_days(ItId, Days) :-
+	iteration(ItId, Start, _),
+	project_iteration(ProjectId, ItId),
+	project(ProjectId, _, _, _, Ndays, _),
+	iteration_days([], Start, Ndays, Days).
+iteration_days(Days, _, Ndays, Days) :- length(Days, Ndays), !.
+iteration_days(Acc, Day, Ndays, Days) :-
+	append(Acc, [Day], NewAcc),
+	NextDay is Day + 86400,
+	iteration_days(NewAcc, NextDay, Ndays, Days).
---
+++ prolog/scenario-1.pl
@@ -0,0 +1,203 @@
+:- use_module(monitoring).
+
+% PROJECT 1
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Start),
+	date_time_stamp(date(2007,7,13,0,0,0,0,-,-), End),
+	assert(project(1, "Project One", Start, End, 7, man_effort)).
+
+% SPRINT 1
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), Start),
+	date_time_stamp(date(2006,7,20,0,0,0,0,-,-), End),
+	assert(iteration(1, Start, End)),
+	assert(project_iteration(1, 1)).
+
+% NEW TASKS FOR SPRINT 1
+:-	assert(task(1, "New Feature", "Cool feature story", [])),
+	assert(project_task(1, 1)),
+	assert(task(2, "New Feature 2", "Cool feature story", [])),
+	assert(project_task(1, 2)),
+	assert(task(3, "New Feature 3", "Cool feature story", [])),
+	assert(project_task(1, 3)),
+	assert(task(4, "New Feature 4", "Cool feature story", [])),
+	assert(project_task(1, 4)).
+
+% TASK UPDATES FOR SPRINT 1
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), When1),
+	assert(task_update(1, 1, When1, 6, 5)),
+	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), When2),
+	assert(task_update(2, 1, When2, 6, 5)),
+	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), When3),
+	assert(task_update(3, 1, When3, 6, 4)),
+	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), When4),
+	assert(task_update(4, 1, When4, 6, 2)),
+	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), When5),
+	assert(task_update(5, 1, When5, 6, 0)).
+
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), When1),
+	assert(task_update(6, 2, When1, 4, 4)),
+	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), When2),
+	assert(task_update(7, 2, When2, 4, 4)),
+	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), When3),
+	assert(task_update(8, 2, When3, 4, 2)),
+	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), When4),
+	assert(task_update(9, 2, When4, 4, 1)),
+	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), When5),
+	assert(task_update(10, 2, When5, 4, 0)).
+
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), When1),
+	assert(task_update(11, 3, When1, 10, 10)),
+	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), When2),
+	assert(task_update(12, 3, When2, 10, 9)),
+	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), When3),
+	assert(task_update(13, 3, When3, 10, 7)),
+	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), When4),
+	assert(task_update(14, 3, When4, 10, 6)),
+	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), When5),
+	assert(task_update(15, 3, When5, 10, 6)),
+	date_time_stamp(date(2006,7,19,0,0,0,0,-,-), When6),
+	assert(task_update(16, 3, When6, 10, 3)),
+	date_time_stamp(date(2006,7,20,0,0,0,0,-,-), When7),
+	assert(task_update(17, 3, When7, 10, 2)).
+
+:- 	date_time_stamp(date(2006,7,14,0,0,0,0,-,-), When1),
+	assert(task_update(18, 4, When1, 8, 7)),
+	date_time_stamp(date(2006,7,15,0,0,0,0,-,-), When2),
+	assert(task_update(19, 4, When2, 8, 6)),
+	date_time_stamp(date(2006,7,16,0,0,0,0,-,-), When3),
+	assert(task_update(20, 4, When3, 8, 7)),
+	date_time_stamp(date(2006,7,17,0,0,0,0,-,-), When4),
+	assert(task_update(21, 4, When4, 8, 6)),
+	date_time_stamp(date(2006,7,18,0,0,0,0,-,-), When5),
+	assert(task_update(22, 4, When5, 8, 5)),
+	date_time_stamp(date(2006,7,19,0,0,0,0,-,-), When6),
+	assert(task_update(23, 4, When6, 8, 3)),
+	date_time_stamp(date(2006,7,20,0,0,0,0,-,-), When7),
+	assert(task_update(24, 4, When7, 8, 1)).
+
+% SPRINT 2
+:- 	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), Start),
+	date_time_stamp(date(2006,7,27,0,0,0,0,-,-), End),
+	assert(iteration(2, Start, End)),
+	assert(project_iteration(1, 2)).
+
+% NEW TASKS FOR SPRINT 2
+:-	assert(task(5, "New Feature 5", "Cool feature story", [])),
+	assert(project_task(1, 5)),
+	assert(task(6, "New Feature 6", "Cool feature story", [])),
+	assert(project_task(1, 6)),
+	assert(task(7, "New Feature 7", "Cool feature story", [])),
+	assert(project_task(1, 7)).
+
+% TASK UPDATES FOR SPRINT 2
+:- 	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), When1),
+	assert(task_update(25, 3, When1, 10, 0)),
+	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), When2),
+	assert(task_update(26, 4, When2, 8, 0)).
+
+:- 	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), When1),
+	assert(task_update(27, 5, When1, 10, 9)),
+	date_time_stamp(date(2006,7,22,0,0,0,0,-,-), When2),
+	assert(task_update(28, 5, When2, 10, 8)),
+	date_time_stamp(date(2006,7,23,0,0,0,0,-,-), When3),
+	assert(task_update(29, 5, When3, 10, 7)),
+	date_time_stamp(date(2006,7,24,0,0,0,0,-,-), When4),
+	assert(task_update(30, 5, When4, 10, 6)),
+	date_time_stamp(date(2006,7,25,0,0,0,0,-,-), When5),
+	assert(task_update(31, 5, When5, 10, 2)),
+	date_time_stamp(date(2006,7,26,0,0,0,0,-,-), When6),
+	assert(task_update(32, 5, When6, 10, 1)),
+	date_time_stamp(date(2006,7,27,0,0,0,0,-,-), When7),
+	assert(task_update(33, 5, When7, 10, 0)).
+
+:- 	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), When1),
+	assert(task_update(34, 6, When1, 6, 6)),
+	date_time_stamp(date(2006,7,22,0,0,0,0,-,-), When2),
+	assert(task_update(35, 6, When2, 6, 5)),
+	date_time_stamp(date(2006,7,23,0,0,0,0,-,-), When3),
+	assert(task_update(36, 6, When3, 6, 2)),
+	date_time_stamp(date(2006,7,24,0,0,0,0,-,-), When4),
+	assert(task_update(37, 6, When4, 6, 0)).
+
+:- 	date_time_stamp(date(2006,7,21,0,0,0,0,-,-), When1),
+	assert(task_update(38, 7, When1, 6, 6)),
+	date_time_stamp(date(2006,7,22,0,0,0,0,-,-), When2),
+	assert(task_update(39, 7, When2, 6, 5)),
+	date_time_stamp(date(2006,7,23,0,0,0,0,-,-), When3),
+	assert(task_update(40, 7, When3, 6, 4)),
+	date_time_stamp(date(2006,7,24,0,0,0,0,-,-), When4),
+	assert(task_update(41, 7, When4, 6, 3)),
+	date_time_stamp(date(2006,7,25,0,0,0,0,-,-), When5),
+	assert(task_update(42, 7, When5, 6, 2)),
+	date_time_stamp(date(2006,7,26,0,0,0,0,-,-), When6),
+	assert(task_update(43, 7, When6, 6, 1)),
+	date_time_stamp(date(2006,7,27,0,0,0,0,-,-), When7),
+	assert(task_update(44, 7, When7, 6, 0)).
+
+% SPRINT 3
+:- 	date_time_stamp(date(2006,7,28,0,0,0,0,-,-), Start),
+	date_time_stamp(date(2006,8,3,0,0,0,0,-,-), End),
+	assert(iteration(3, Start, End)),
+	assert(project_iteration(1, 3)).
+
+% NEW TASKS FOR SPRINT 2
+:-	assert(task(8, "New Feature 8", "Cool feature story", [])),
+	assert(project_task(1, 8)),
+	assert(task(9, "New Feature 9", "Cool feature story", [])),
+	assert(project_task(1, 9)),
+	assert(task(10, "New Feature 10", "Cool feature story", [])),
+	assert(project_task(1, 10)),
+	assert(task(11, "New Feature 11", "Cool feature story", [])),
+	assert(project_task(1, 11)).
+
+% TASK UPDATES FOR SPRINT 3
+:- 	date_time_stamp(date(2006,7,28,0,0,0,0,-,-), When1),
+	assert(task_update(45, 8, When1, 10, 9)),
+	date_time_stamp(date(2006,7,29,0,0,0,0,-,-), When2),
+	assert(task_update(46, 8, When2, 10, 8)),
+	date_time_stamp(date(2006,7,30,0,0,0,0,-,-), When3),
+	assert(task_update(47, 8, When3, 10, 7)),
+	date_time_stamp(date(2006,7,31,0,0,0,0,-,-), When4),
+	assert(task_update(48, 8, When4, 10, 6)),
+	date_time_stamp(date(2006,8,1,0,0,0,0,-,-), When5),
+	assert(task_update(49, 8, When5, 10, 4)),
+	date_time_stamp(date(2006,8,2,0,0,0,0,-,-), When6),
+	assert(task_update(50, 8, When6, 10, 3)),
+	date_time_stamp(date(2006,8,3,0,0,0,0,-,-), When7),
+	assert(task_update(51, 8, When7, 10, 1)).
+
+:- 	date_time_stamp(date(2006,7,28,0,0,0,0,-,-), When1),
+	assert(task_update(52, 9, When1, 7, 6)),
+	date_time_stamp(date(2006,7,29,0,0,0,0,-,-), When2),
+	assert(task_update(53, 9, When2, 7, 6)),
+	date_time_stamp(date(2006,7,30,0,0,0,0,-,-), When3),
+	assert(task_update(54, 9, When3, 7, 5)),
+	date_time_stamp(date(2006,7,31,0,0,0,0,-,-), When4),
+	assert(task_update(55, 9, When4, 7, 2)),
+	date_time_stamp(date(2006,8,1,0,0,0,0,-,-), When5),
+	assert(task_update(56, 9, When5, 7, 2)),
+	date_time_stamp(date(2006,8,2,0,0,0,0,-,-), When6),
+	assert(task_update(57, 9, When6, 7, 0)).
+
+:-	date_time_stamp(date(2006,7,28,0,0,0,0,-,-), When1),
+	assert(task_update(58, 10, When1, 4, 3)),
+	date_time_stamp(date(2006,7,29,0,0,0,0,-,-), When2),
+	assert(task_update(59, 10, When2, 4, 2)),
+	date_time_stamp(date(2006,7,30,0,0,0,0,-,-), When3),
+	assert(task_update(60, 10, When3, 4, 1)),
+	date_time_stamp(date(2006,7,31,0,0,0,0,-,-), When4),
+	assert(task_update(61, 10, When4, 4, 0)).
+
+:- 	date_time_stamp(date(2006,7,28,0,0,0,0,-,-), When1),
+	assert(task_update(62, 11, When1, 12, 12)),
+	date_time_stamp(date(2006,7,29,0,0,0,0,-,-), When2),
+	assert(task_update(63, 11, When2, 12, 11)),
+	date_time_stamp(date(2006,7,30,0,0,0,0,-,-), When3),
+	assert(task_update(64, 11, When3, 12, 10)),
+	date_time_stamp(date(2006,7,31,0,0,0,0,-,-), When4),
+	assert(task_update(65, 11, When4, 12, 8)),
+	date_time_stamp(date(2006,8,1,0,0,0,0,-,-), When5),
+	assert(task_update(66, 11, When5, 12, 6)),
+	date_time_stamp(date(2006,8,2,0,0,0,0,-,-), When6),
+	assert(task_update(67, 11, When6, 12, 5)),
+	date_time_stamp(date(2006,8,3,0,0,0,0,-,-), When7),
+	assert(task_update(68, 11, When7, 12, 4)).
